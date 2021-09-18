d98814ff2fdb818915c141d30c427bd025a2e484
Christophe Ghisi <christophe.ghisi@gmail.com>
2021-09-18 18:27:17 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -24,7 +24,7 @@ const MenuLinks = ({ links = [] }) => {
 const Footer = ({ footer }) => {
   return (
     <footer className="footer bg-gray-600 relative pt-1 border-b-2 border-gray-500 text-white">
-      <div className="container md:mx-auto mx-10">
+      <div className="container mx-auto">
         <div className="flex my-5">
           <div className="flex-1">
             <h3>{RichText.asText(footer.data.title)}</h3>
