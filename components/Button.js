ba4e07ece845782e1bd997f420294e409d664df2
christophe ghisi <christophe.ghisi@cantire.com>
2022-08-11 16:42:47 -0400

    condition update
    design issue on empty url buttons

--- components/Button.js
+++ components/Button.js
@@ -7,11 +7,13 @@ export const Button = ({ primary, backgroundColor, size, label, url, ...props })
     ? "border-2 border-black py-4 px-5 hover:bg-black hover:text-white font-medium"
     : "border-2 border-black bg-black text-white py-4 px-5 hover:bg-white hover:text-black font-medium";
 
-  return (
-    <Link
-      {...props}
-      href={hrefResolver(url)} passHref>
-      <a className={[`btn--${size}`, mode].join(" ")}>{label}</a>
-    </Link>
-  );
+  if (url.url){
+    return (
+      <Link
+        {...props}
+        href={hrefResolver(url)} passHref>
+        <a className={[`btn--${size}`, mode].join(" ")}>{label}</a>
+      </Link>
+    );
+  }
 };
