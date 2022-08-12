57f5c7b5ce55f6fc2951b34899103c4de13c0794
christophe ghisi <christophe.ghisi@cantire.com>
2022-08-12 13:46:27 -0400

    missing ALT

--- components/MobileNavigation.js
+++ components/MobileNavigation.js
@@ -32,6 +32,7 @@ class MobileNavigation extends Component {
             className="fill-current h-5 w-5"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
+            alt="navigation"
           >
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
