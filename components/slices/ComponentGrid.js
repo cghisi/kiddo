0efd72f3beef575126131f0a4ac66e166e7907e7
Christophe Ghisi <christophe.ghisi@gmail.com>
2022-10-03 20:17:25 +0000

    Update ComponentGrid.js

--- components/slices/ComponentGrid.js
+++ components/slices/ComponentGrid.js
@@ -1,34 +1,34 @@
 import { RichText } from "prismic-reactjs";
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 
 import { Button } from "../Button";
 
 export default function ComponentGrid({ slice }) {
-    return (
-        <section className="container mx-auto">
-            <h2 className="text-3xl text-center font-bold my-5">{RichText.asText(slice.primary.heading)}</h2>
-            <div className="flex flex-col md:flex-row">
-                {slice.items.map((item, index) => (
-                    <div
-                        key={index}
-                        className="md:w-1/2 text-center m-5"
-                    >
-                        <Image
-                            src={item.image.url}
-                            alt={item.image.alt}
-                            width={650}
-                            height={650}
-                        />
-                        <div className="md:text-left md:mx-10 my-10 text-center">
-                            <b>{RichText.asText(item.title)}</b> - {RichText.asText(item.description)}
-                        </div>
-                        <div className="text-center">
-                            <Button primary label={item.cta_title} url={item.url}/>
-                        </div>
-                    </div>
-                ))}
+  return (
+    <section className="container mx-auto">
+      <h2 className="text-3xl text-center font-bold my-5">
+        {RichText.asText(slice.primary.heading)}
+      </h2>
+      <div className="flex flex-col lg:flex-row">
+        {slice.items.map((item, index) => (
+          <div key={index} className="lg:w-1/2 text-center m-5">
+            <Image
+              src={item.image.url}
+              alt={item.image.alt}
+              width={650}
+              height={650}
+            />
+            <div className="lg:text-left my-10 text-center">
+              <b>{RichText.asText(item.title)}</b> -{" "}
+              {RichText.asText(item.description)}
             </div>
-        </section>
-    );
+            <div className="text-center">
+              <Button primary label={item.cta_title} url={item.url} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
